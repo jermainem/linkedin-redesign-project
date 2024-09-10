cbce3f8364dbced07511ac1733f757e6b18502e6
Jermaine Miller <jmillerjr93@gmail.com>
2024-09-09 20:40:14 -0400

    Added Tailwind and Fontawesome

---
+++ client/src/assets/fonts/fontawesome.ts
@@ -0,0 +1,6 @@
+// src/fontawesome.ts
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+library.add(faMagnifyingGlass, faLinkedin);
